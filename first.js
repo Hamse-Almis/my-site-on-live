3ecbab7585c74fdb805330cea4d61f75559be451
Hamse-Almis <79413980+Hamse-Almis@users.noreply.github.com>
2021-03-07 02:51:51 -1200

    co
    my first comment

---
+++ first.js
@@ -0,0 +1,8 @@
+
+function add(){
+var num1 =document.getElementById("txtuser");
+var num2 =document.getElementById("txtpass");
+var result =document.getElementById("result");
+
+result.innerText=num1(num1.value)+num2(num2.value);
+}
